a3126d1796946a91890191339ac0a9e73ad8c448
תכנות <@690156GUR.LOCAL>
2023-05-28 18:05:31 +0300

    change connect to connectSql

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const { HOST, PORT } = process.env;
 connectMng().then(_ => {
     connectSql().then(_ => {
         createTables().then(_ => {
-            // createProcedures().then(_ => {
+            createProcedures().then(_ => {
                 // createSpecialProcedures().then(_ => {
                     app.listen(PORT, HOST, () => {
                         console.log(`http://${HOST}:${PORT}`);
                     });
                 // })
-            // });
+            });
         });
     });
 });
