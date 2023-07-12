79530744fe3103a0852fca55edfbbd909869dd3d
Rut Broyner <@690156GUR.LOCAL>
2023-07-12 16:09:42 +0300

    add function to convert query or body to mongo filter

--- utils/convert_condition.js
+++ utils/convert_condition.js
@@ -1,8 +1,17 @@
 const convertToSqlQuery = (condition) => {
-    
-}
-const convertToMongoFilter = () => {
 
+}
+const convertToMongoFilter = (condition) => {
+        let subFilter = {}
+        for (let key in condition) {
+            if (condition[key] instanceof Array) {
+                subFilter[`$${key.toLowerCase()}`] = condition[key].map(o => convertToMongoFilter(o))
+            }
+            else {
+                subFilter[key] = condition[key]
+            }
+        }
+        return subFilter
 }
 
 module.exports = { convertToMongoFilter, convertToSqlQuery }
