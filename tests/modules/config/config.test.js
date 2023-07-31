6ebc90e58dcc1aa6825323a4c08cbfaf02b514e5
Y. Charaz <yeuditc@skyvar.co.il>
2023-07-31 15:50:02 +0300

    Basic template for testing

--- tests/modules/config/config.test.js
+++ tests/modules/config/config.test.js
@@ -1,5 +1,6 @@
 const {getTableFromConfig, getCollectionsFromConfig, readJoin, getReferencedColumns, setFullObj, getTableAccordingToRef, getObjectWithFeildNameForPrimaryKey, getForeignTableAndColumn,  convertFieldType, getTabeColumnName} = require('../../../modules/config/config')
 
+
 describe('config.js', () => {
     describe('GET TABLE FROM CONFIG', () => { 
         it('A function received a table name and returns the table accordingly' ,()=>{
@@ -46,12 +47,81 @@ describe('config.js', () => {
             expect(result).toBeDefined();
         }); 
     });
-    describe('READ JOIN', () => { });
-    describe('GET REFERENCED COLUMNS', () => { });
-    describe('SET FULL OBJ', () => { });
-    describe('GET TABLE ACCORDING TO REF', () => { });
-    describe('GET OBJECT WITH FIELD NAME FOR PRIMARY KEY', () => { });
-    describe('GET FOREIGN TABLE AND COLUMN', () => { });
-    describe('CONVERT FIELD TYPE', () => { });
-    describe('GET TABLE COLUMN NAME', () => { });
+
+    // describe('READ JOIN', () => { });
+
+    describe('GET REFERENCED COLUMNS', () => {
+        it('A function is given a table name and returns the column name and its reference' ,()=>{
+            const result = getReferencedColumns();
+            expect(result).toBeDefined();
+        }); 
+        it('The function received a value (table name)' ,()=>{
+            const result = getReferencedColumns();
+            expect(result).toBeDefined();
+        }); 
+        it('The value returned is of type object' ,()=>{
+            const result = getReferencedColumns();
+            expect(result).toBeDefined();
+        }); 
+        it('The table name should be of type string' ,()=>{
+            const result = getReferencedColumns();
+            expect(result).toBeDefined();
+        }); 
+        it('table name does not exist returns an error accordingly' ,()=>{
+            const result = getReferencedColumns();
+            expect(result).toBeDefined();
+        }); 
+        it('The column name does not have a reference and returns an error accordingly' ,()=>{
+            const result = getReferencedColumns();
+            expect(result).toBeDefined();
+        }); 
+    });
+
+    // describe('SET FULL OBJ', () => { });
+    
+    describe('GET TABLE ACCORDING TO REF', () => {
+        it('A function received a table name and returns the name of the table to which it is directed' ,()=>{
+            const result = getTableAccordingToRef();
+            expect(result).toBeDefined();
+        }); 
+        it('The function received a value (table name)' ,()=>{
+            const result = getTableAccordingToRef();
+            expect(result).toBeDefined();
+        });
+        it('The value returned is of type object' ,()=>{
+            const result = getTableAccordingToRef();
+            expect(result).toBeDefined();
+        }); 
+        it('The table name should be of type string' ,()=>{
+            const result = getTableAccordingToRef();
+            expect(result).toBeDefined();
+        }); 
+        it('table name does not exist returns an error accordingly' ,()=>{
+            const result = getTableAccordingToRef();
+            expect(result).toBeDefined();
+        }); 
+    });
+
+    // describe('GET OBJECT WITH FIELD NAME FOR PRIMARY KEY', () => { });
+
+    describe('GET FOREIGN TABLE AND COLUMN', () => {
+        it('' ,()=>{
+            const result = getForeignTableAndColumn();
+            expect(result).toBeDefined();
+        }); 
+     });
+
+    describe('CONVERT FIELD TYPE', () => {
+        it('' ,()=>{
+            const result = convertFieldType();
+            expect(result).toBeDefined();
+        }); 
+     });
+
+    describe('GET TABLE COLUMN NAME', () => {
+        it('' ,()=>{
+            const result = getTabeColumnName();
+            expect(result).toBeDefined();
+        }); 
+     });
 })
