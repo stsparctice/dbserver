9ebf15bbd1c44d49dc33904093a725cc2a43e442
LeahGuterman <LeaGu@skyvar.co.il>
2023-07-31 14:12:50 +0300

    config.test file template

---
+++ tests/modules/config/config.test.js
@@ -0,0 +1,13 @@
+describe('config.js', () => {
+    describe('getTableFromConfig', () => { })
+    describe('getCollectionsFromConfig', () => { })
+    describe('readJoin', () => { })
+    describe('getReferencedColumns', () => { })
+    describe('setFullObj', () => { })
+    describe('getTables', () => { })
+    describe('getTableAccordingToRef', () => { })
+    describe('getObjectWithFeildNameForPrimaryKey', () => { })
+    describe('getForeignTableAndColumn', () => { })
+    describe('convertFieldType', () => { })
+    describe('getTabeColumnName', () => { })
+})
