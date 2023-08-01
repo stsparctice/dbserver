3653282bb2d4cf6b6028cc5ca9ee86e27f27c393
Y. Charaz <yeuditc@skyvar.co.il>
2023-08-01 15:46:00 +0300

    Correct getColumns function and Tests

--- tests/modules/config/createConfig.test.js
+++ tests/modules/config/createConfig.test.js
@@ -12,24 +12,40 @@ describe('TEST ON createConfig.js FILE', () => {
     //     });
     // });
 
-    describe('GET COLUMNS', () => {
+    describe('GET COLUMNS',() => {
         it('The function accepts a table name and returns its columns' ,()=>{
             const result = getColumns('tbl_example_table1',config);
             expect(result).toBeDefined();
-            expect(result).toStrictEqual();
+            expect(result).toStrictEqual( [
+                {
+                    "name": "Id",
+                    "sqlName": "Id",
+                    "type": "INT IDENTITY PRIMARY KEY NOT NULL"
+                },
+                {
+                    "name": "measure",
+                    "sqlName": "Measure",
+                    "type": "NVARCHAR(20) NOT NULL "
+                },
+                {
+                    "name": "disable",
+                    "sqlName": "Disable",
+                    "type": "BIT NOT NULL"
+                }
+            ]);
         });
         it('The table name is of type string' ,()=>{
-            expect(() => getColumns(15,config)).toThrow();
-            expect(() => getColumns("tbl_example_table5",config)).not.toThrow();
+            expect(() => getColumns(tbl_example_table1,config)).toThrow();
+            expect(() => getColumns("tbl_example_table1",config)).not.toThrow();
+        });
+        it('The returned value is of type array' ,()=>{
+            const result = getColumns('tbl_example_table1',config);
+            expect(result).toBeDefined();
+            expect(result).toBeInstanceOf(Array)
         });
         it('The table name exists in the config' ,()=>{
             expect(() => getColumns("tbl_example_table6",config)).toThrow();
-            expect(() => getColumns("tbl_example_table5",config)).not.toThrow();
-        });
-        it('The returned value is of type array' ,()=>{
-            const result = getColumns('',config);
-            expect(result).toBeDefined();
-            expect(result).toBeInstanceOf(Array)
+            expect(() => getColumns("tbl_example_table1",config)).not.toThrow();
         });
         it('Invalid config file' ,()=>{
             expect(() => getColumns("tbl_example_table5",incorrectConfig)).toThrow();
